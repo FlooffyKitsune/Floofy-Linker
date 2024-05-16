8debe73f29fa48830ff4b683846578507e384cfb
Jarrett Dominic <wafflekat1123@gmail.com>
2024-05-15 23:26:24 -0400

    Form validation and live updates implemented

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import { auth, user } from "$lib/firebase";
+  import { auth, user } from "$lib/firebase";
 
-    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-    async function signInWithGoogle() {
-        const provider = new GoogleAuthProvider();
-        const user = await signInWithPopup(auth, provider);
-        console.log(user);
-    }
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(user);
+  }
 </script>
 
 <h2>Login</h2>
