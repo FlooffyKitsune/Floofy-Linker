eacb5be858c3565c2701a084770588bbf93e584e
FlooffyKitsune <wafflekat1123@gmail.com>
2024-05-21 14:35:03 -0400

    Added a temporary home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Link } from 'svelte-routing';
+</script>
+
+<svelte:head>
+    <title>Floofy Linker</title>
+</svelte:head>
+
+<main>
+    <h1>Welcome to Floofy Linker</h1><br/>
+    <p>This is a work in progress project of a site meant to house links for a registered user.  While bio editing is not currently functional and the site is still being built out and styled, you can still register and try it out!</p>
+    <br>
+    <a class="btn btn-primary" href="/login">Get Started</a>
+</main>
